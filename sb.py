2275056d9cc95995199a0d1db3118bd2c31c4ef6
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2024-09-13 14:46:01 +0800

    sb.py

--- sb.py
+++ sb.py
@@ -1,11 +1,11 @@
 import requests
-import os
 import urllib.parse
+import os
 
-# 原始配置 URL
+# 配置 URL
 config_url = "https://raw.githubusercontent.com/yaney01/chromego_merge/main/sub/shadowrocket_base64.txt"
 
-# Sing-Box 配置 URL
+# Sing-Box 配置服务 URL
 base_url = "https://singbox.nyc.mn/singbox"
 
 # 对配置 URL 进行 URL 编码
@@ -15,23 +15,25 @@ encoded_config_url = urllib.parse.quote(config_url)
 request_url = f"{base_url}?config={encoded_config_url}"
 
 # 发送 HTTP GET 请求
-response = requests.get(request_url)
+try:
+    response = requests.get(request_url)
+    response.raise_for_status()  # 如果响应状态码不是 200，将引发 HTTPError
+    if response.status_code == 200:
+        # 确保输出文件夹存在
+        output_folder = "sub"
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
-# 检查请求是否成功
-if response.status_code == 200:
-    # 确保输出文件夹存在
-    output_folder = "sub"
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+        # 构建输出文件路径
+        output_filename = "sb_singbox.json"
+        output_path = os.path.join(output_folder, output_filename)
 
-    # 构建输出文件路径
-    output_filename = "sb_singbox.json"
-    output_path = os.path.join(output_folder, output_filename)
+        # 将转换后的内容写入文件
+        with open(output_path, 'w', encoding='utf-8') as file:
+            file.write(response.text)
 
-    # 将转换后的内容写入文件
-    with open(output_path, 'w', encoding='utf-8') as file:
-        file.write(response.text)
-
-    print(f"成功将内容写入 {output_path}")
-else:
-    print(f"请求失败，状态码: {response.status_code}")
+        print(f"成功将内容写入 {output_path}")
+    else:
+        print(f"请求失败，状态码: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"请求出现错误: {e}")
