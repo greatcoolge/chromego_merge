43058845afd3695739a2c2827bdbe43f617003e1
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2024-09-15 14:03:56 +0800

    meta_merge.py

--- meta_merge.py
+++ meta_merge.py
@@ -355,7 +355,7 @@ merged_proxies = []
 # Process the URLs
 process_urls('./urls/clash_urls.txt', process_clash)
 # process_urls('./urls/sb_urls.txt', process_sb)
-process_urls('./urls/clashmeta.txt', process_clash)
+# process_urls('./urls/clashmeta.txt', process_clash)
 process_urls('./urls/hysteria_urls.txt', process_hysteria)
 process_urls('./urls/hysteria2_urls.txt', process_hysteria2)
 process_urls('./urls/xray_urls.txt', process_xray)
